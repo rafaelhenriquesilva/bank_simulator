2c2e43fcddb1d48317ead9f48d7b05a633ad56e3
Rafael Silva <rafaelhenriquesilva494@gmail.com>
2023-10-14 12:26:41 -0300

    feature: production any test

--- src/test/end-to-end/helpers/login.helper.ts
+++
@@ -1,8 +0,0 @@
-export const loginUser = (request: any, username: string, password: string) => {
-  return request.post('/login')
-    .send({
-      "username": username,
-      "password": password
-    })
-    .set('Content-Type', 'application/json'); // Set the content-type header
-}
